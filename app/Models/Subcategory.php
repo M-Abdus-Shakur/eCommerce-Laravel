205411c67ec2cff69a5d6079b9ec08852ddf3fdd
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-01-28 22:59:17 +0600

    category, subcategory Model fillable added

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Subcategory extends Model
 {
     use HasFactory;
+
+    protected $fillable =
+    [
+        'subcategory_name',
+        'category_id',
+        'category_name',
+        'slug',
+    ];
 }

aeba0e99854ea2e6224288a2b1f320e24baa2c1d
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-01-28 23:54:55 +0600

    get all category added

--- app/Http/Livewire/AllCategoriesComponent.php
+++ app/Http/Livewire/AllCategoriesComponent.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class AllCategoriesComponent extends Component
 {
+    public $categories;
     public function render()
     {
+        $this->categories = Category::get();
         return view('livewire.all-categories-component');
     }
 }

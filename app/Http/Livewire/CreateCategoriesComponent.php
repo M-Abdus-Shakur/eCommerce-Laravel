3bf3995b809356597c3bd10c1ecb7e4009d41d2f
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-01-28 23:47:25 +0600

    add category added

--- app/Http/Livewire/CreateCategoriesComponent.php
+++ app/Http/Livewire/CreateCategoriesComponent.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class CreateCategoriesComponent extends Component
 {
+    public $category_name;
+    public function addcategory(){
+        $this->validate([
+            'category_name' => 'required|unique:categories',
+        ]);
+
+        Category::create([
+            'category_name' => $this->category_name,
+            'slug' => str()->slug($this->category_name),
+        ]);
+        session()->flash('success','Category Added Successfully');
+        $this->reset();
+    }
     public function render()
     {
         return view('livewire.create-categories-component');
     }
+
 }
--- resources/views/livewire/create-categories-component.blade.php
+++ resources/views/livewire/create-categories-component.blade.php
@@ -1,3 +1,19 @@
 <div>
-    <h1>Create Categories </h1>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <form wire:submit.prevent="addcategory">
+                        @include('livewire._flash_message')
+                        <div class="mb-2">
+                            <label for="defaultFormControlInput" class="form-label">Category Name</label>
+                            <input type="text" class="form-control" placeholder="Enter Your Category Name!" wire:model="category_name" />
+                            @error('category_name') <span class="text-danger">{{ $message }}</span> @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>

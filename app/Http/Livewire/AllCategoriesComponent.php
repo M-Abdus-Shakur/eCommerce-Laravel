292bfd7e1df569ea149d858d50edfaad28533be0
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-01-29 12:10:26 +0600

    category delete button added

--- app/Http/Livewire/AllCategoriesComponent.php
+++ app/Http/Livewire/AllCategoriesComponent.php
@@ -8,6 +8,13 @@ use Livewire\Component;
 class AllCategoriesComponent extends Component
 {
     public $categories;
+
+    public function deleteCategory($id)
+    {
+        Category::where('id',$id)->delete();
+        session()->flash('success','Category Deleted Successfully');
+    }
+
     public function render()
     {
         $this->categories = Category::get();
--- resources/views/livewire/all-categories-component.blade.php
+++ resources/views/livewire/all-categories-component.blade.php
@@ -5,6 +5,7 @@
         <h5 class="card-header">Striped rows</h5>
         <div class="table-responsive text-nowrap">
             <table class="table table-striped">
+                @include('livewire._flash_message')
                 <thead>
                     <tr>
                         <th>Category Name</th>
@@ -21,7 +22,7 @@
                             <td>{{$category->product_count}}</td>
                             <td>
                                 <a class="p-2" href=""><i class="bx bx-edit-alt me-1"></i>Edit</a>
-                                <a class="p-2" href=""><i class="bx bx-trash me-1"></i>Delete</a>
+                                <a class="p-2" href="javascript:void(0)" wire:click="deleteCategory({{$category->id}})"><i class="bx bx-trash me-1"></i>Delete</a>
                             </td>
                         </tr>
                     </tbody>

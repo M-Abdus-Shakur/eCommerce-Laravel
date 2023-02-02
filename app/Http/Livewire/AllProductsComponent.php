1337af7e7727df7be77c91fe3ea4f934e68ce37f
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-02-02 12:58:35 +0600

    delete product image while deleting product added

--- app/Http/Livewire/AllProductsComponent.php
+++ app/Http/Livewire/AllProductsComponent.php
@@ -5,17 +5,23 @@ namespace App\Http\Livewire;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Subcategory;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class AllProductsComponent extends Component
 {
     public $products;
 
-    public function deleteProduct($id,$product_category_id,$product_subcategory_id)
+    public function deleteProduct($id, $product_category_id, $product_subcategory_id)
     {
+        $product_image = Product::findOrFail($id)->product_image;
+        if (File::exists(public_path('storage/' . $product_image))) {
+            unlink('storage/' . $product_image);
+        }
         Product::where('id', $id)->delete();
-        Category::findOrFail($product_category_id)->decrement('product_count',1);
-        Subcategory::findOrFail($product_subcategory_id)->decrement('product_count',1);
+        Category::findOrFail($product_category_id)->decrement('product_count', 1);
+        Subcategory::findOrFail($product_subcategory_id)->decrement('product_count', 1);
         session()->flash('success', 'Product Deleted Successfully');
     }
 

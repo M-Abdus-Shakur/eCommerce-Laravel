15d40f5f115b8dd03dade08b6d31a7af0b903ebc
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-02-03 19:41:27 +0600

    all product added with pagination

--- app/Http/Livewire/Users/AllProductsComponent.php
+++ app/Http/Livewire/Users/AllProductsComponent.php
@@ -5,16 +5,17 @@ namespace App\Http\Livewire\Users;
 use App\Models\Category;
 use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AllProductsComponent extends Component
 {
     public $categories;
-    public $products;
 
     public function render()
     {
         $this->categories = Category::all();
-        $this->products = Product::get();
-        return view('livewire.users.all-products-component')->layout('layouts.user');
+        return view('livewire.users.all-products-component',[
+            'products' => Product::paginate(1),
+        ])->layout('layouts.user');
     }
 }

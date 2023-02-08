8c3b0ecd34c8c50501aad35b7193d43b34768716
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-02-08 21:14:24 +0600

    similar product view code delted

--- app/Http/Livewire/Users/ViewSingleProductComponent.php
+++ app/Http/Livewire/Users/ViewSingleProductComponent.php
@@ -9,6 +9,7 @@ use Livewire\Component;
 class ViewSingleProductComponent extends Component
 {
     public $categories;
+    public $product_similar_subcategory;
     public $product , $product_id;
 
     public function mount($id)
@@ -20,6 +21,8 @@ class ViewSingleProductComponent extends Component
     {
         $this->categories = Category::all();
         $this->product = Product::findOrFail($this->product_id)->first();
+        $product_subcategory_id =  $this->product->product_subcategory_id;
+        $this->product_similar_subcategory = Product::where('product_subcategory_id',$product_subcategory_id)->get();
         return view('livewire.users.view-single-product-component')->layout('layouts.user');
     }
 }
